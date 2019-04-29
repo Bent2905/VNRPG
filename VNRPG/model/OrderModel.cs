f04c725a9239d197677a10c50f33037e45163f72
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 01:00:09 +0700

    Create OrderModel.cs

---
+++ VNRPG/model/OrderModel.cs
@@ -0,0 +1,13 @@
+using GTANetworkAPI;
+
+namespace VNRPG.model
+{
+    public class OrderModel
+    {
+        public int id { get; set; }
+        public Vector3 position { get; set; }
+        public float distance { get; set; }
+        public double limit { get; set; }
+        public bool taken { get; set; }
+    }
+}
