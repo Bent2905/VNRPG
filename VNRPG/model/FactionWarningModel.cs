aaf32c7cb79cef4d7ce423fdb885b778a22cbb1f
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:58:09 +0700

    Create FactionWarningModel.cs

---
+++ VNRPG/model/FactionWarningModel.cs
@@ -0,0 +1,25 @@
+using GTANetworkAPI;
+using System;
+
+namespace VNRPG.model
+{
+    public class FactionWarningModel
+    {
+        public int faction { get; set; }
+        public int playerId { get; set; }
+        public string place { get; set; }
+        public Vector3 position { get; set; }
+        public int takenBy { get; set; }
+        public string hour { get; set; }
+
+        public FactionWarningModel(int faction, int playerId, string place, Vector3 position, int takenBy, string hour)
+        {
+            this.faction = faction;
+            this.playerId = playerId;
+            this.place = place;
+            this.position = position;
+            this.takenBy = takenBy;
+            this.hour = hour;
+        }
+    }
+}
