883c1b291ffbfc5745e27e19c5c5030da6e1b162
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:59:38 +0700

    Create InventoryModel.cs

---
+++ VNRPG/model/InventoryModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace VNRPG.model
+{
+    public class InventoryModel
+    {
+        public int id { get; set; }
+        public string hash { get; set; }
+        public string description { get; set; }
+        public int type { get; set; }
+        public int amount { get; set; }
+    }
+}
