348d11696241f2c485b01397f952ee43cdd23b64
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:56:46 +0700

    Create ContactModel.cs

---
+++ VNRPG/model/ContactModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace VNRPG.model
+{
+    public class ContactModel
+    {
+        public int id { get; set; }
+        public int owner { get; set; }
+        public int contactNumber { get; set; }
+        public string contactName { get; set; }
+    }
+}
