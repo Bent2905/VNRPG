0026d0e9a65f896eecb2bb56a00b0da25306f629
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 01:01:37 +0700

    Create TestModel.cs

---
+++ VNRPG/model/TestModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VNRPG.model
+{
+    public class TestModel
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public int question { get; set; }
+    }
+}
