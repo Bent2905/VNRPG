37e504d5e59899643edad52ddfa6bf448e0c2a3a
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:53:43 +0700

    Create AdminTicketModel.cs

---
+++ VNRPG/model/AdminTicketModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace VNRPG.model
+{
+    public class AdminTicketModel
+    {
+        public int playerId { get; set; }
+        public string question { get; set; }
+    }
+}
