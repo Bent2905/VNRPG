a6e1ba7676caf5a167f7020955eeb4b52f6d6fce
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 01:01:54 +0700

    Create TunningModel.cs

---
+++ VNRPG/model/TunningModel.cs
@@ -0,0 +1,10 @@
+namespace VNRPG.model
+{
+    public class TunningModel
+    {
+        public int id { get; set; }
+        public int vehicle { get; set; }
+        public int slot { get; set; }
+        public int component { get; set; }
+    }
+}
