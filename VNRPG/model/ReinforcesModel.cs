fa304b79e0bc89f7923dd6eafe92788c6b24ec63
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 01:01:01 +0700

    Create ReinforcesModel.cs

---
+++ VNRPG/model/ReinforcesModel.cs
@@ -0,0 +1,16 @@
+using GTANetworkAPI;
+
+namespace VNRPG.model
+{
+    public class ReinforcesModel
+    {
+        public int playerId { get; set; }
+        public Vector3 position { get; set; }
+
+        public ReinforcesModel(int playerId, Vector3 position)
+        {
+            this.playerId = playerId;
+            this.position = position;
+        }
+    }
+}
