1ef0b9acf72e6556e1293f6462dc751be625f561
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:58:47 +0700

    Create GarbageModel.cs

---
+++ VNRPG/model/GarbageModel.cs
@@ -0,0 +1,18 @@
+using GTANetworkAPI;
+
+namespace VNRPG.model
+{
+    public class GarbageModel
+    {
+        public int route { get; set; }
+        public int checkPoint { get; set; }
+        public Vector3 position { get; set; }
+
+        public GarbageModel(int route, int checkPoint, Vector3 position)
+        {
+            this.route = route;
+            this.checkPoint = checkPoint;
+            this.position = position;
+        }
+    }
+}
