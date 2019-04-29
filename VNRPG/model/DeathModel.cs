94dffe46ca1d661fe94ecf01609c13615ad81bb1
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:57:38 +0700

    Create DeathModel.cs

---
+++ VNRPG/model/DeathModel.cs
@@ -0,0 +1,18 @@
+using GTANetworkAPI;
+
+namespace VNRPG.model
+{
+    public class DeathModel
+    {
+        public Client player { get; set; }
+        public Client killer { get; set; }
+        public uint weapon { get; set; }
+
+        public DeathModel(Client player, Client killer, uint weapon)
+        {
+            this.player = player;
+            this.killer = killer;
+            this.weapon = weapon;
+        }
+    }
+}
