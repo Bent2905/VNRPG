4895873d2434ee0a6852e1ed31e521830a32a873
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:54:54 +0700

    Create BankOperationModel.cs

---
+++ VNRPG/model/BankOperationModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace VNRPG.model
+{
+    public class BankOperationModel
+    {
+        public string source { get; set; }
+        public string receiver { get; set; }
+        public string type { get; set; }
+        public int amount { get; set; }
+        public string day { get; set; }
+        public string time { get; set; }
+    }
+}
