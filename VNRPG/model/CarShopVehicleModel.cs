74e0b89e0c01e62dd752e188556182d6b61d2c45
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:56:20 +0700

    Create CarShopVehicleModel.cs

---
+++ VNRPG/model/CarShopVehicleModel.cs
@@ -0,0 +1,22 @@
+using GTANetworkAPI;
+
+namespace VNRPG.model
+{
+    public class CarShopVehicleModel
+    {
+        public VehicleHash hash { get; set; }
+        public int carShop { get; set; }
+        public int type { get; set; }
+        public int speed { get; set; }
+        public int price { get; set; }
+        public string model { get; set; }
+
+        public CarShopVehicleModel(VehicleHash hash, int carShop, int type, int price)
+        {
+            this.hash = hash;
+            this.carShop = carShop;
+            this.type = type;
+            this.price = price;
+        }
+    }
+}
