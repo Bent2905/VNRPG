ccb2ec916fdf768416e6317ca7d7ec7a02ae7c4b
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:57:21 +0700

    Create CrateSpawnModel.cs

---
+++ VNRPG/model/CrateSpawnModel.cs
@@ -0,0 +1,16 @@
+using GTANetworkAPI;
+
+namespace VNRPG.model
+{
+    public class CrateSpawnModel
+    {
+        public int spawnPoint { get; set; }
+        public Vector3 position { get; set; }
+
+        public CrateSpawnModel(int spawnPoint, Vector3 position)
+        {
+            this.spawnPoint = spawnPoint;
+            this.position = position;
+        }
+    }
+}
