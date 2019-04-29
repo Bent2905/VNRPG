8ac847924a27fea1c893fd2da77987ddb833d229
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:58:56 +0700

    Create GunModel.cs

---
+++ VNRPG/model/GunModel.cs
@@ -0,0 +1,19 @@
+using GTANetworkAPI;
+using System;
+
+namespace VNRPG.model
+{
+    public class GunModel
+    {
+        public WeaponHash weapon { get; set; }
+        public string ammunition { get; set; }
+        public int capacity { get; set; }
+
+        public GunModel(WeaponHash weapon, string ammunition, int capacity)
+        {
+            this.weapon = weapon;
+            this.ammunition = ammunition;
+            this.capacity = capacity;
+        }
+    }
+}
