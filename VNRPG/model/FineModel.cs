f84015ba3d74757ef31c2eddb40625f6e7aa71d2
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:58:28 +0700

    Create FineModel.cs

---
+++ VNRPG/model/FineModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace VNRPG.model
+{
+    public class FineModel
+    {
+        public string officer { get; set; }
+        public string target { get; set; }
+        public int amount { get; set; }
+        public string reason { get; set; }
+        public string date { get; set; }
+    }
+}
