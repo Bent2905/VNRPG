2f8989e00e85f46835342a9b7c77b7a4b96e96fc
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 01:00:23 +0700

    Create ParkingModel.cs

---
+++ VNRPG/model/ParkingModel.cs
@@ -0,0 +1,14 @@
+using GTANetworkAPI;
+
+namespace VNRPG.model
+{
+    public class ParkingModel
+    {
+        public int id { get; set; }
+        public int type { get; set; }
+        public int houseId { get; set; }
+        public Vector3 position { get; set; }
+        public int capacity { get; set; }
+        public TextLabel parkingLabel { get; set; }
+    }
+}
