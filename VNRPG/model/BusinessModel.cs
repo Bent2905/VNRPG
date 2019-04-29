7130a5ee88230524f9833dd67cfc4a1895c311a2
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:55:55 +0700

    Create BusinessModel.cs

---
+++ VNRPG/model/BusinessModel.cs
@@ -0,0 +1,21 @@
+using GTANetworkAPI;
+
+namespace VNRPG.model
+{
+    public class BusinessModel
+    {
+        public int id { get; set; }
+        public int type { get; set; }
+        public string ipl { get; set; }
+        public string name { get; set; }
+        public Vector3 position { get; set; }
+        public uint dimension { get; set; }
+        public string owner { get; set; }
+        public int funds { get; set; }
+        public int products { get; set; }
+        public float multiplier { get; set; }
+        public bool locked { get; set; }
+        public TextLabel businessLabel { get; set; }
+        public Checkpoint businessCheckpoint { get; set; }
+    }
+}
