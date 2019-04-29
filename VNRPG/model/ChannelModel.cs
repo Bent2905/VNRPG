18d651392eb5ca9deb20c45285899d077fbea678
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:56:28 +0700

    Create ChannelModel.cs

---
+++ VNRPG/model/ChannelModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VNRPG.model
+{
+    public class ChannelModel
+    {
+        public int id { get; set; }
+        public int owner { get; set; }
+        public string password { get; set; }
+    }
+}
