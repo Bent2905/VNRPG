1c4b428107462d06476b15b60ee4ea1ee0b61e45
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 01:01:23 +0700

    Create TattooModel.cs

---
+++ VNRPG/model/TattooModel.cs
@@ -0,0 +1,10 @@
+namespace VNRPG.model
+{
+    public class TattooModel
+    {
+        public int player { get; set; }
+        public int slot { get; set; }
+        public string library { get; set; }
+        public string hash { get; set; }
+    }
+}
