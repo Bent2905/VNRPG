748dd58387d1972578b9fb8f8162a2155d417f8c
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:58:00 +0700

    Create FactionModel.cs

---
+++ VNRPG/model/FactionModel.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace VNRPG.model
+{
+    public class FactionModel
+    {
+        public string descriptionMale { get; set; }
+        public string descriptionFemale { get; set; }
+        public int faction { get; set; }
+        public int rank { get; set; }
+        public int salary { get; set; }
+
+        public FactionModel(string descriptionMale, string descriptionFemale, int faction, int rank, int salary)
+        {
+            this.descriptionMale = descriptionMale;
+            this.descriptionFemale = descriptionFemale;
+            this.faction = faction;
+            this.rank = rank;
+            this.salary = salary;
+        }
+    }
+}
