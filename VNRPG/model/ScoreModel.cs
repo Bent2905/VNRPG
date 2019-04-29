3bae6471ddd51833996a3eeeb0252845fd33885c
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 01:01:08 +0700

    Create ScoreModel.cs

---
+++ VNRPG/model/ScoreModel.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace VNRPG.model
+{
+    public class ScoreModel
+    {
+        public int playerId { get; set; }
+        public string playerName { get; set; }
+        public int playerPing { get; set; }
+
+        public ScoreModel(int playerId, string playerName, int playerPing)
+        {
+            this.playerId = playerId;
+            this.playerName = playerName;
+            this.playerPing = playerPing;
+        }
+    }
+}
