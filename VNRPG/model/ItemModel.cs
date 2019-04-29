8425156a284fc1eab370677563acd414f6fc85e3
Bent2905 <43984346+Bent2905@users.noreply.github.com>
2019-04-30 00:59:47 +0700

    Create ItemModel.cs

---
+++ VNRPG/model/ItemModel.cs
@@ -0,0 +1,32 @@
+using GTANetworkAPI;
+
+namespace VNRPG.model
+{
+    public class ItemModel
+    {
+        public int id { get; set; }
+        public string hash { get; set; }
+        public string ownerEntity { get; set; }
+        public int ownerIdentifier { get; set; }
+        public int amount { get; set; }
+        public Vector3 position { get; set; }
+        public uint dimension { get; set; }
+        public Object objectHandle { get; set; }
+
+        public ItemModel Copy()
+        {
+            ItemModel itemModel = new ItemModel();
+
+            itemModel.id = id;
+            itemModel.hash = hash;
+            itemModel.ownerEntity = ownerEntity;
+            itemModel.ownerIdentifier = ownerIdentifier;
+            itemModel.amount = amount;
+            itemModel.position = position;
+            itemModel.dimension = dimension;
+            itemModel.objectHandle = objectHandle;
+
+            return itemModel;
+        }
+    }
+}
